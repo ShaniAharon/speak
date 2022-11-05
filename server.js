d12bcf800e25ac707c8e444eb7ba1bfdffb97bdf
shforeal <rivena14798@gmail.com>
2022-11-05 21:57:59 +0200

    'fix'

--- server.js
+++ server.js
@@ -17,16 +17,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.static('public'))
-if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.resolve(__dirname, 'public')))
-}
-// else {
-//   const corsOptions = {
-//     origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-//     credentials: true
-//   }
-//   app.use(cors(corsOptions))
-// }
+
 
 // const authRoutes = require('./api/auth/auth.routes')
 // const userRoutes = require('./api/user/user.routes')
