b9bcbde05831e0bb97d76c1fb8f84d1897c0a33c
shforeal <rivena14798@gmail.com>
2022-11-05 22:00:09 +0200

    'fix-path'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-// const path = require('path')
+const path = require('path')
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
@@ -17,7 +17,16 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.static('public'))
-
+if (process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.resolve(__dirname, 'public')))
+}
+// else {
+//   const corsOptions = {
+//     origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+//     credentials: true
+//   }
+//   app.use(cors(corsOptions))
+// }
 
 // const authRoutes = require('./api/auth/auth.routes')
 // const userRoutes = require('./api/user/user.routes')
